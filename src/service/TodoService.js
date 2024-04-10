21393fa26a66d924abbfc3e9d73fad84cf26dc05
student-vedansh <35373105+ebe25@users.noreply.github.com>
2024-04-10 07:02:41 +0530

    build(server):init config

--- src/service/TodoService.js
+++ src/service/TodoService.js
@@ -1,40 +1,36 @@
 import TodoRepositary from "../repositary/todoRepo.js";
 
+class TodoService {
+  constructor() {
+    this.repo = new TodoRepositary();
+  }
 
-class TodoService{
-    constructor(){
-        this.repo= new TodoRepositary();
+  async create(data) {
+    try {
+      const newToDo = await this.repo.create(data);
+      return newToDo;
+    } catch (error) {
+      console.log("error", error);
+      throw {error};
     }
-
-async create (data){
+  }
+  async get(userEmail) {
     try {
-        const newToDo = await this.repo.create(data);
-        return newToDo;
-      } catch (error) {
-        console.log("error", error);
-        throw {error};
-      }
-
-}
-async get (userEmail){
+      const newToDo = await this.repo.get(userEmail);
+      return newToDo;
+    } catch (error) {
+      console.log("error", error);
+      throw {error};
+    }
+  }
+  async getAll() {
     try {
-        const newToDo = await this.repo.get(userEmail);
-        return newToDo;
-      } catch (error) {
-        console.log("error", error);
-        throw {error};
-      }
-
-}
-async getAll (){
-    try {
-        const newToDo = await this.repo.getAll();
-        return newToDo;
-      } catch (error) {
-        console.log("error", error);
-        throw {error};
-      }
-
-}
+      const newToDo = await this.repo.getAll();
+      return newToDo;
+    } catch (error) {
+      console.log("error", error);
+      throw {error};
+    }
+  }
 }
 export default TodoService;
